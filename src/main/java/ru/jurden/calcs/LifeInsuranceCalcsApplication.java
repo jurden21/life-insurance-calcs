6d81eaef8f413ac0449da18e4d1b6cab91742adc
jurden <dev@jurden.ru>
2024-05-27 23:21:57 +0300

    Initial commit

---
+++ src/main/java/ru/jurden/calcs/LifeInsuranceCalcsApplication.java
@@ -0,0 +1,13 @@
+package ru.jurden.calcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeInsuranceCalcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeInsuranceCalcsApplication.class, args);
+    }
+
+}
