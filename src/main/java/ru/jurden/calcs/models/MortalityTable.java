8e9fa4b92fdd5ed8a8dc4145e5b7e696da4caaac
jurden <dev@jurden.ru>
2025-04-07 22:44:38 +0300

    Add MortalityTable and tests

--- src/main/java/ru/jurden/calcs/models/MortalityTable.java
+++ src/main/java/ru/jurden/calcs/models/MortalityTable.java
@@ -19,19 +19,44 @@ public class MortalityTable {
     private final Map<Integer, Integer> lx;
     private final Map<Integer, Integer> dx;
 
-    public int getLx(int x) {
-        return lx.getOrDefault(x, 0);
-    }
-
-    public int getDx(int x) {
-        return dx.getOrDefault(x, 0);
-    }
-
     public int getMinAge() {
         return lx.keySet().stream().min(Integer::compareTo).orElseThrow();
     }
 
     public int getMaxAge() {
         return lx.keySet().stream().max(Integer::compareTo).orElseThrow();
+    }
+
+    /**
+     * <p>l<sub>x</sub> - показатель таблицы смертности, характеризующий число лиц из наблюдаемой совокупности,
+     * доживших до возраста x лет. Значения l приводятся в таблице смертности при целых x (x = 0, 1, 2,.. w,
+     * где w - предельный возраст таблицы смертности).</p>
+     */
+    public int getLx(int x) {
+        return lx.getOrDefault(x, 0);
+    }
+
+    /**
+     * <p>d<sub>x</sub> = l<sub>x</sub> - l<sub>x+1</sub> - показатель таблицы смертности, характеризующий
+     * число лиц, умерших в возрасте от x лет до возраста x+1 год.</p>
+     */
+    public int getDx(int x) {
+        return dx.getOrDefault(x, 0);
+    }
+
+    /**
+     * <p><sub>n</sub>p<sub>x</sub> = l<sub>x+n</sub> / l<sub>x</sub> - вероятность для лица в возрасте x лет
+     * дожить до возраста x+n лет.</p>
+     */
+    public double getPnx(int x, int n) {
+        return 1.0 * getLx(x + n) / getLx(x);
+    }
+
+    /**
+     * <p><sub>n</sub>q<sub>x</sub> = (l<sub>x</sub> - l<sub>x+n</sub>) / l<sub>x</sub> - вероятность для лица
+     * в возрасте x лет умереть в течение предстоящих n лет.</p>
+     */
+    public double getQnx(int x, int n) {
+        return 1.0 * (getLx(x) - getLx(x + n)) / getLx(x);
     }
 }
