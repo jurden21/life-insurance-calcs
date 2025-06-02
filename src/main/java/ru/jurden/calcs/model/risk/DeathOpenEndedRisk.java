aabcf2873600726cad24752fe401c758e4131cf3
jurden <dev@jurden.ru>
2025-06-02 22:44:17 +0300

    Add Death Open Ended Risk

---
+++ src/main/java/ru/jurden/calcs/model/risk/DeathOpenEndedRisk.java
@@ -0,0 +1,49 @@
+package ru.jurden.calcs.model.risk;
+
+import ru.jurden.calcs.model.CalculationContext;
+import ru.jurden.calcs.enums.ContractType;
+import ru.jurden.calcs.enums.MortalityTableType;
+import ru.jurden.calcs.enums.RiskType;
+import ru.jurden.calcs.model.InterestRate;
+import ru.jurden.calcs.model.MortalityTable;
+import ru.jurden.calcs.model.Risk;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DeathOpenEndedRisk implements Risk {
+
+    private final InterestRate interestRate;
+    private final Map<MortalityTableType, MortalityTable> mortalityTables;
+
+    public DeathOpenEndedRisk(InterestRate interestRate, List<MortalityTable> mortalityTables) {
+        this.interestRate = interestRate;
+        this.mortalityTables = mortalityTables.stream()
+                .collect(Collectors.toMap(MortalityTable::getTableType,
+                        mortalityTable -> mortalityTable));
+    }
+
+    @Override
+    public RiskType riskType() {
+        return RiskType.DEATH;
+    }
+
+    @Override
+    public ContractType contractType() {
+        return ContractType.OPEN_ENDED;
+    }
+
+    @Override
+    public Double calculate(CalculationContext context) {
+        MortalityTable mortalityTable = mortalityTables.get(context.getMortalityTableType());
+
+        double result = 0;
+        for (int i = context.getAge(); i < mortalityTable.getMaxAge(); i++) {
+            result += Math.pow(interestRate.getV(), i + 1.0) * mortalityTable.getDx(i);
+        }
+        result = result / mortalityTable.getLx(context.getAge()) / Math.pow(interestRate.getV(), context.getAge());
+        result = result * interestRate.getI() / interestRate.getB();
+        return result;
+    }
+}
---
+++ src/test/java/ru/jurden/calcs/model/risk/DeathOpenEndedRiskTest.java
@@ -0,0 +1,74 @@
+package ru.jurden.calcs.model.risk;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.jurden.calcs.enums.ContractType;
+import ru.jurden.calcs.enums.MortalityTableType;
+import ru.jurden.calcs.enums.RiskType;
+import ru.jurden.calcs.model.CalculationContext;
+import ru.jurden.calcs.model.InterestRate;
+import ru.jurden.calcs.model.MortalityTable;
+import ru.jurden.calcs.table.MortalityTableFactory;
+import ru.jurden.calcs.table.MortalityTableParser;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DeathOpenEndedRiskTest {
+
+    private static final double DELTA = 1e-10;
+
+    private final MortalityTableParser parser = new MortalityTableParser();
+    private final MortalityTableFactory factory = new MortalityTableFactory(parser);
+    private DeathOpenEndedRisk risk;
+
+    @Test
+    void riskType() {
+        InterestRate interestRate = new InterestRate(5);
+        MortalityTable mortalityTable = factory.loadTable(MortalityTableType.MALE);
+        risk = new DeathOpenEndedRisk(interestRate, List.of(mortalityTable));
+        RiskType riskType = risk.riskType();
+        assertEquals("DEATH", riskType.name());
+    }
+
+    @Test
+    void contractType() {
+        InterestRate interestRate = new InterestRate(5);
+        MortalityTable mortalityTable = factory.loadTable(MortalityTableType.MALE);
+        risk = new DeathOpenEndedRisk(interestRate, List.of(mortalityTable));
+        ContractType contractType = risk.contractType();
+        assertEquals("OPEN_ENDED", contractType.name());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDataToCalculate")
+    void calculate(double percent, MortalityTableType mortalityTableType, int age, double value) {
+        InterestRate interestRate = new InterestRate(percent);
+        MortalityTable mortalityTable = factory.loadTable(mortalityTableType);
+        risk = new DeathOpenEndedRisk(interestRate, List.of(mortalityTable));
+        CalculationContext context = new CalculationContext()
+                .setAge(age)
+                .setMortalityTableType(mortalityTableType);
+        assertEquals(value, risk.calculate(context), DELTA);
+    }
+
+    static Stream<Arguments> provideDataToCalculate() {
+        return Stream.of(
+                Arguments.of(3.0, MortalityTableType.MALE, 40, 0.4895763275),
+                Arguments.of(5.0, MortalityTableType.MALE, 40, 0.3321822628),
+                Arguments.of(3.0, MortalityTableType.MALE, 50, 0.5895305369),
+                Arguments.of(5.0, MortalityTableType.MALE, 50, 0.4406219594),
+                Arguments.of(3.0, MortalityTableType.FEMALE, 40, 0.3707700000),
+                Arguments.of(5.0, MortalityTableType.FEMALE, 40, 0.2104903450),
+                Arguments.of(3.0, MortalityTableType.FEMALE, 50, 0.4738485963),
+                Arguments.of(5.0, MortalityTableType.FEMALE, 50, 0.3084783949),
+                Arguments.of(3.0, MortalityTableType.MIXED, 40, 0.3959137093),
+                Arguments.of(5.0, MortalityTableType.MIXED, 40, 0.2357463379),
+                Arguments.of(3.0, MortalityTableType.MIXED, 50, 0.4983452486),
+                Arguments.of(5.0, MortalityTableType.MIXED, 50, 0.3361510741));
+    }
+}
